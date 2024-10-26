822908b2d242fff5226b9ece9ec5e36ce980e4b2
m-xuanquy <hi.maixuanquy@gmail.com>
2024-10-26 09:38:16 +0700

    Added Product page

--- src/winui/Sale_Project/ViewModels/ProductViewModel.cs
+++ src/winui/Sale_Project/ViewModels/ProductViewModel.cs
@@ -1,4 +1,8 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Sale_Project.Contracts.ViewModels;
+using Sale_Project.Core.Contracts.Services;
+using Sale_Project.Core.Models;
 
 namespace Sale_Project.ViewModels;
 
