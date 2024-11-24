1e2b70deb444646fdf2d3e4cc06b71799457fe99
html <22120436@student.hcmus.edu.vn>
2024-11-24 20:21:25 +0700

    complete add search fields

--- src/winui/Sale_Project/ViewModels/EmployeeViewModel.cs
+++ src/winui/Sale_Project/ViewModels/EmployeeViewModel.cs
@@ -5,30 +5,72 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using Sale_Project.Contracts.ViewModels;
 using Sale_Project.Core.Contracts.Services;
 using Sale_Project.Core.Models;
+using Sale_Project.Core.Models.Employee;
 
 namespace Sale_Project.ViewModels;
 
 public partial class EmployeeViewModel : ObservableRecipient, INavigationAware
 {
     //private readonly ISampleDataService _sampleDataService;
+    private const int _defaultRowsPerPage = 10;
 
-    public ObservableCollection<Employee> Source { get; } = new ObservableCollection<Employee>();
+
+    public ObservableCollection<Employee> Employees { get; } = new ObservableCollection<Employee>();
+
+    [ObservableProperty]
+    private EmployeeSearchRequest employeeSearchRequest;
+
+    public string Info => $"Displaying {Employees.Count}/{RowsPerPage} of total {TotalItems} item(s)";
+
+    public ObservableCollection<PageInfo> PageInfos
+    {
+        get; set;
+    }
+    public PageInfo SelectedPageInfoItem
+    {
+        get; set;
+    }
+    //public string Keyword { get; set; } = "";
+    public int CurrentPage
+    {
+        get; set;
+    } = 1;
+    public int TotalPages
+    {
+        get; set;
+    }
+    public int TotalItems { get; set; } = 0;
+    public int RowsPerPage
+    {
+        get; set;
+    }
+
+    public string SortField
+    {
+        get; set;
+    } = "id";
+    public SortType SortType
+    {
+        get; set;
+    } = SortType.ASC;
+
 
     public EmployeeViewModel(/*ISampleDataService sampleDataService*/)
     {
-        //_sampleDataService = sampleDataService;
+        RowsPerPage = _defaultRowsPerPage;
+        EmployeeSearchRequest = new EmployeeSearchRequest();
     }
 
     public async void OnNavigatedTo(object parameter)
     {
-        Source.Clear();
+        Employees.Clear();
 
         // TODO: Replace with real data.
         //var data = await _sampleDataService.GetGridDataAsync();
 
         //foreach (var item in data)
         //{
-        //    Source.Add(item);
+        //    Employees.Add(item);
         //}
     }
 
--- src/winui/Sale_Project/Views/EmployeePage.xaml.cs
+++ src/winui/Sale_Project/Views/EmployeePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.UI.Xaml.Controls;
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 
 using Sale_Project.ViewModels;
 
@@ -17,5 +18,54 @@ public sealed partial class EmployeePage : Page
     {
         ViewModel = App.GetService<EmployeeViewModel>();
         InitializeComponent();
+    }
+
+    private void AddEmployeeButton_Click(object sender, RoutedEventArgs e)
+    {
+        //ViewModel.AddEmployee();
+    }
+
+    private async void UnemployedEmployeeButton_Click(object sender, RoutedEventArgs e)
+    {
+        //if (itemsDataGrid.SelectedItem == null)
+        //{
+        //    await _dialogService.ShowErrorAsync("Error", "Please select an employee to delete");
+        //    return;
+        //}
+
+        //var employee = itemsDataGrid.SelectedItem as Employee;
+        //var success = ViewModel.UnemployedEmployee(employee);
+
+        //if (success)
+        //{
+        //    await _dialogService.ShowSuccessAsync("Success", "Employee is unemployed successfully");
+        //}
+        //else
+        //{
+        //    await _dialogService.ShowErrorAsync("Error", "Failed to unemployed employee");
+        //}
+    }
+
+    private async void UpdateEmployeeButton_Click(object sender, RoutedEventArgs e)
+    {
+        //if (itemsDataGrid.SelectedItem == null)
+        //{
+        //    await _dialogService.ShowErrorAsync("Error", "Please select an employee to update");
+        //    return;
+        //}
+
+        //var employee = itemsDataGrid.SelectedItem as Employee;
+
+        //ViewModel.UpdateEmployee(employee);
+    }
+
+    private void PreviousButton_Click(object sender, RoutedEventArgs e)
+    {
+        //ViewModel.GoToPreviousPage();
+    }
+
+    private void NextButton_Click(object sender, RoutedEventArgs e)
+    {
+        //ViewModel.GoToNextPage();
     }
 }
