65fe7ae33bf59c6c31524b8b686b319aef1f509e
html <22120436@student.hcmus.edu.vn>
2024-12-11 13:47:54 +0700

    delete some commented code

--- src/winui/Sale_Project/ViewModels/AccountViewModel.cs
+++ src/winui/Sale_Project/ViewModels/AccountViewModel.cs
@@ -46,34 +46,6 @@ public partial class AccountViewModel : ObservableRecipient
     }
 
     /// <summary>
-    /// Retrieves the full file path for a given JSON file located in the "MockData" directory of the Sale_Project.
-    /// </summary>
-    /// <param name="fileName">The name of the JSON file to retrieve the path for.</param>
-    /// <returns>
-    /// The full file path to the specified JSON file.
-    /// </returns>
-    /// <exception cref="InvalidOperationException">
-    /// Thrown if the path to the "Sale_Project" directory cannot be found.
-    /// </exception>
-    //public string GetJsonFilePath(string fileName)
-    //{
-    //    var fullPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-
-    //    var index = fullPath.IndexOf(@"Sale_Project");
-
-    //    if (index != -1)
-    //    {
-    //        var basePath = fullPath.Substring(0, index);
-
-    //        return Path.Combine(basePath, @"Sale_Project\StaticData\", fileName);
-    //    }
-    //    else
-    //    {
-    //        throw new InvalidOperationException("Invalid path");
-    //    }
-    //}
-
-    /// <summary>
     /// Loads account data asynchronously by retrieving the account information from the authentication service.
     /// </summary>
     /// <remarks>
@@ -109,14 +81,6 @@ public partial class AccountViewModel : ObservableRecipient
     {
         try
         {
-            //if (string.IsNullOrWhiteSpace(oldPassword) || string.IsNullOrWhiteSpace(newPassword))
-            //{
-            //    await _dialogService.ShowErrorAsync(
-            //        "Login Failed",
-            //        "Username and password cannot be empty.");
-            //    return;
-            //}
-
             var result = await _authService.ChangePasswordAsync(oldPassword, newPassword);
             if (result)
             {
@@ -124,12 +88,7 @@ public partial class AccountViewModel : ObservableRecipient
                     "Success",
                     "Password changed successfully!");
             }
-            //else
-            //{
-            //    await _dialogService.ShowErrorAsync(
-            //        "Change Password Failed",
-            //        "Wrong password.");
-            //}
+
         }
         catch (Exception ex)
         {
